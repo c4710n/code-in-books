856e65dbf4b0b709928f6a18f8b47654cd2595e0
c4710n <c4710n@users.noreply.github.com>
2022-08-08 00:04:06 +0800

    feat(metaprogramming-elixir): complete 11-hub

--- metaprogramming-elixir/11-hub/README.md
+++ metaprogramming-elixir/11-hub/README.md
@@ -1,21 +1,20 @@
 # Hub
 
-**TODO: Add description**
+- At compile-time, `Hub` caches GitHub data directly within function definitions.
+- At runtime, `Hub` provides a function `go/1` to open corresponding link in browser (macOS only).
 
-## Installation
+## Setup
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `hub` to your list of dependencies in `mix.exs`:
-
-```elixir
-def deps do
-  [
-    {:hub, "~> 0.1.0"}
-  ]
-end
+```sh
+$ mix deps.get
 ```
 
-Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
-and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
-be found at <https://hexdocs.pm/hub>.
+## Play
 
+```sh
+$ iex -S mix
+
+iex> Hub.
+
+iex> Hub.go(:"code-in-books")
+```
--- metaprogramming-elixir/11-hub/lib/hub.ex
+++ metaprogramming-elixir/11-hub/lib/hub.ex
@@ -3,16 +3,27 @@ defmodule Hub do
   Documentation for `Hub`.
   """
 
-  @doc """
-  Hello world.
+  @username "c4710n"
 
-  ## Examples
+  "https://api.github.com/users/#{@username}/repos"
+  |> then(&Finch.build(:get, &1, [{"content-type", "application/json"}]))
+  |> Finch.request(JustFinch)
+  |> case do
+    {:ok, %Finch.Response{body: body, status: 200}} ->
+      Jason.decode!(body)
 
-      iex> Hub.hello()
-      :world
+    _ ->
+      raise "failed to request ;("
+  end
+  |> Enum.each(fn repo ->
+    def unquote(String.to_atom(repo["name"]))() do
+      unquote(Macro.escape(repo))
+    end
+  end)
 
-  """
-  def hello do
-    :world
+  def go(repo) do
+    url = apply(__MODULE__, repo, [])["html_url"]
+    IO.puts("Launching browser to #{url}...")
+    System.cmd("open", [url])
   end
 end
